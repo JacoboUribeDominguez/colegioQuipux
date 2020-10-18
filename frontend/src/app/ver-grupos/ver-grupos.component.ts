21b4baf4a3324617eba63a427913a0ca3f5def38
JakoPapaFrita <jacobouribe@inemjose.edu.co>
2020-10-17 23:16:26 -0500

    updating.. (3)

--- frontend/src/app/ver-grupos/ver-grupos.component.ts
+++ frontend/src/app/ver-grupos/ver-grupos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ver-grupos',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerGruposComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  NavegarHaciaInicio(){
+    this.router.navigate(['/']);
   }
 
 }
